d282c402c7de3ecf79289434af4b543044485f67
sandraindiasl <indiaslopezsandra@gmail.com>
2022-07-15 11:14:39 +0200

    Enviar emails

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -2,18 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\MessageReceived;
+use Illuminate\Support\Facades\Mail;
+
 class MessagesController extends Controller
 {
     public function store()
     {
-        Request()->validate([
+        $message = request()->validate([
             'name' => 'required',
             'email' => 'required|email',
             'subject' => 'required',
             'content' => 'required|min:3'
-
+        ], [
+                'name.required' => __('Introduce tu nombre')
         ]);
 
-        return 'Datos validados';
+        Mail::to('indiaslopezsandra@gmail.com')->queue(new MessageReceived($message));
+
+        return 'Mensaje enviado';
     }
 }
