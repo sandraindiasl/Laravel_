977ba09b4c1654d6f03d0e0a0a1ed217b515f6f1
sandraindiasl <indiaslopezsandra@gmail.com>
2022-07-17 18:28:47 +0200

    Actualizar registros

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Project;
 use Illuminate\Http\Request;
-use App\Http\Requests\CreateProjectRequest;
+use App\Http\Requests\SaveProjectRequest;
 
 class ProjectController extends Controller
 {
@@ -32,7 +32,7 @@ class ProjectController extends Controller
         return view('projects.create');
     }
 
-    public function store(CreateProjectRequest $request)
+    public function store(SaveProjectRequest $request)
     {
         Project::create($request->validated() );
 
@@ -40,5 +40,18 @@ class ProjectController extends Controller
 
     }
 
+    public function edit(Project $project)
+    {
+         return view('projects.edit',
+            [
+                'project' => $project
+            ]);
+    }
 
+    public function update(Project $project, SaveProjectRequest $request)
+    {
+        $project->update( $request->validated() );
+
+        return redirect()->route('projects.show', '$project');                                                                  
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -53,6 +53,9 @@ Route::view('/quienes-somos', 'about')->name('about');
 
 Route::get('/portafolio', 'ProjectController@index')->name('projects.index');
 Route::get('/portafolio/crear', 'ProjectController@create')->name('projects.create');
+Route::get('/portafolio/{project}/editar', 'ProjectController@edit')->name('projects.edit');
+
+Route::patch('/portafolio/{project}', 'ProjectController@update')->name('projects.update');
 
 Route::post('/portafolio', 'ProjectController@store')->name('projects.store');
 
