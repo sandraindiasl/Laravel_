93350e3b8be08d4c1bbc3d40a6844282b9220b61
sandraindiasl <indiaslopezsandra@gmail.com>
2022-07-18 21:40:38 +0200

    Cambiar rutas

--- routes/web.php
+++ routes/web.php
@@ -51,18 +51,9 @@ Route::get('/', function() {
 Route::view('/', 'home')->name('home');
 Route::view('/quienes-somos', 'about')->name('about');
 
-Route::get('/portafolio', 'ProjectController@index')->name('projects.index');
-Route::get('/portafolio/crear', 'ProjectController@create')->name('projects.create');
-
-Route::get('/portafolio/{project}/editar', 'ProjectController@edit')->name('projects.edit');
-Route::patch('/portafolio/{project}', 'ProjectController@update')->name('projects.update');
-
-Route::delete('/portafolio/{project}','ProjectController@destroy')->name('projects.destroy');
-
-Route::post('/portafolio', 'ProjectController@store')->name('projects.store');
-
-Route::get('/portafolio/{project}', 'ProjectController@show')->name('projects.show');
-
+Route::resource('portafolio', 'ProjectController')
+->parameters(['portafolio'=>'project'])
+->names('projects');
 
 Route::view('/contacto', 'contact')->name('contact');
 Route::post('contact', 'MessagesController@store')->name('messages.store');
